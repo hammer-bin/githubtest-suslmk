147d14305b7d53dc36706f64d1ed400ee3579014
이민규 <67575226+suslmk-lee@users.noreply.github.com>
2024-08-13 10:33:15 +0900

    Initial Commit

---
+++ githubtest-suslmk/githubtest_suslmkApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubtest_suslmkApp.swift
+//  githubtest-suslmk
+//
+//  Created by Minkyu Lee on 8/13/24.
+//
+
+import SwiftUI
+
+@main
+struct githubtest_suslmkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
